c0beddda535c1cae4d5dfb6ca8fff61ce7a62a37
ac-reynolds <adam.chiu.reynolds@gmail.com>
2021-01-05 09:46:08 -0800

    Implements sample movement
    - adds adam scene
    - implements event system
    - implements input handler
    - implements player controller

---
+++ Assets/Scripts/Controls/InputHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Global input handler that handles all input.  Every Update(), it will take input and effect the
+ * appropriate changes in game state.  
+ * 
+ * Place a single instance of this script on any permanent object in the game. 
+ */
+public class InputHandler : MonoBehaviour
+{
+
+    //for handling inputs other than default movement
+    public enum InputAction
+    {
+        Interact
+    }
+    private static Dictionary<InputAction, KeyCode> keyBinds = new Dictionary<InputAction, KeyCode> {
+        { InputAction.Interact,   KeyCode.E }
+    };
+
+    //controller currently being manipulated by player movement commands
+    private PlayerController _activeController;
+
+    /*
+     * As soon as this object is created (before Start()s are called), register this object to listen for
+     * new SetActiveControllerEvent instances.
+     */
+    private void OnEnable() {
+        EventHandler<SetActiveControllerEvent>.GetInstance().AddListener(OnSetActiveControllerEvent);
+    }
+    private void OnDisable() {
+        EventHandler<SetActiveControllerEvent>.GetInstance().RemoveListener(OnSetActiveControllerEvent);
+    }
+
+    /*
+     * Every frame, get vertical/horizontal inputs (WASD by default) and pass them to the player controller.
+     */
+    private void Update() {
+        if (_activeController == null) {
+            Debug.Log("Input handler has no active controller.  Invoke SetActiveControllerEvent to set the active controller.");
+        } else {
+            _activeController.RequestMovement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+
+    private void OnSetActiveControllerEvent(SetActiveControllerEvent e) {
+        _activeController = e.Controller;
+    }
+}
---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.Events;
+
+/*
+ * Generic event handler for events.  Use AddListener and RemoveListener to modify
+ * listeners.  Use Invoke to invoke the event.  Any data passed to invoke will be
+ * sent to listeners.  
+ * 
+ * Access this globally with GetInstance(), e.g.:
+ * EventHandler<SetActiveControllerEvent>.GetInstance().AddListener(OnSetActiveControllerEvent);
+ */
+public class EventHandler<T>
+{
+    private static EventHandler<T> instance = new EventHandler<T>();
+    public static EventHandler<T> GetInstance() {
+        return instance;
+    }
+
+    private UnityEvent<T> _event;
+    private EventHandler() {
+        if (_event == null) {
+            _event = new UnityEvent<T>();
+        }
+    }
+
+    //passes args to anyone registered to receive a T
+    public void Invoke(T args) {
+        _event.Invoke(args);
+    }
+
+    public void AddListener(UnityAction<T> action) {
+        _event.AddListener(action);
+    }
+
+    public void RemoveListener(UnityAction<T> action) {
+        _event.RemoveListener(action);
+    }
+}
